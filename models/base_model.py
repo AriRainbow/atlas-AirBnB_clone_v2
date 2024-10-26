b6ff1ba01604ae49cd7d9489652c6f08eed6d983
AriRainbow <mariel.williams@atlasschool.com>
2024-10-25 21:52:08 -0500

    6. DBStorage - States and Cities: base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,43 +2,56 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime, timezone
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+#Define Base for SQLAlchemy if DBSTorage is active
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    # SQLAlchemy columns (only if DBStorage is active)
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.timezone.utc)
+    updated_at = Column(DateTime, nullable=False, default=datetime.timezone.utc)
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        """Instatntiates a new model with SQLAlchemy compatibility"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now(timezone.utc)
+        self.updated_at = datetime.now(timezone.utc)
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls = self.__class__.__name__
+        return f'[{cls}] ({self.id}) {self.__dict__}'
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now(timezone.utc)  # timezone-aware datetime
+        storage.new(self)
         storage.save()
 
+    def delete(self):
+        """Deletes current instance from storage"""
+        from models import storage
+        storage.delete(self)
+
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        """Convert instance into dict format, excluding SQLAlchemy state"""
+        dictionary = {key: (value if not isinstance(value, datetime)
+                            else value.isoformat())
+                    for key, value in self.__dict__.items()
+                    if key != '_sa_instance_state'}
+        dictionary['__class__'] = self.__class__.__name__
         return dictionary
