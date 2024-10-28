f41d153d8aaa6a38e36d05cbf1ab62a33d11c753
AriRainbow <mariel.williams@atlasschool.com>
2024-10-27 23:31:25 -0500

    Revise for timezone consistancy

--- models/base_model.py
+++ models/base_model.py
@@ -14,21 +14,21 @@ class BaseModel:
 
     # SQLAlchemy columns (only if DBStorage is active)
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.timezone.utc)
-    updated_at = Column(DateTime, nullable=False, default=datetime.timezone.utc)
+    created_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model with SQLAlchemy compatibility"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now(timezone.utc)
-        self.updated_at = datetime.now(timezone.utc)
-
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+                if key in ['created_at', 'updated_at']:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now(timezone.utc)
+            self.updated_at = datetime.now(timezone.utc)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -49,9 +49,7 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format, excluding SQLAlchemy state"""
-        dictionary = {key: (value if not isinstance(value, datetime)
-                            else value.isoformat())
-                    for key, value in self.__dict__.items()
-                    if key != '_sa_instance_state'}
-        dictionary['__class__'] = self.__class__.__name__
+        dictionary = {key: (value.isoformat() if isinstance(value, datetime) else value)
+                      for key, value in self.__dict__.items()
+                      if key != '_sa_instance_state'}
         return dictionary
