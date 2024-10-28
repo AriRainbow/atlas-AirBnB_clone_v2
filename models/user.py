6dbfb57c9957dfd44cd98c4e988f8e9c9b7a7986
AriRainbow <mariel.williams@atlasschool.com>
2024-10-27 20:21:04 -0500

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This module defines a User class for the database model."""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents a user for a MySQL database.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Users.
+        email (sqlalchemy String): The user's email address (cannot be null).
+        password (sqlalchemy String): The user's password (cannot be null).
+        first_name (sqlalchemy String): The user's first name (nullable).
+        last_name (sqlalchemy String): The user's last name (nullable).
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
