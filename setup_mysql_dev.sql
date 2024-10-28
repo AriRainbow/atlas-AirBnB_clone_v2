f5b8c136299ea9760a6ec15004ca7e29bbae1c99
AriRainbow <mariel.williams@atlasschool.com>
2024-10-28 02:04:29 -0500

    Update FK

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -28,5 +28,5 @@ DROP TABLE IF EXISTS cities;
 CREATE TABLE cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(128) NOT NULL
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    CONSTRAINT fk_state FOREIGN KEY (state_id) REFERENCES states(id)
 );
