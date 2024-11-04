998699b774489606dd7b97de9162583ca6f2a990
AriRainbow <mariel.williams@atlasschool.com>
2024-11-03 20:54:29 -0600

    Error: DBStorage creates a new Session after close()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,8 +64,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = scoped_session(session_factory)()  # Create a new scoped session
 
     def drop_all(self):
         """ Drops all tables in the database """
